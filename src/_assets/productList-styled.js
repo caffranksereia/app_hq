fe7c5c5949fc4cd8c67a151e6864eefa6690e082
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-22 15:45:08 -0300

    create file _assets and sub file header-styled, index.css, productList-styled using styled components

---
+++ src/_assets/productList-styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Div = styled.div`
+display: flex;
+width: 100%;
+height:500px:
+background: yellow;
+`;
+
+export const DivImage = styled.div`
+width: 250px:
+height: 150px;
+background: yellow;
+`;
