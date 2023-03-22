b7efc954621a4382c69fcfed9fe7be6f17b13ff0
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-22 15:43:11 -0300

    deleted index.css and app and add assets index.css and feature home/ home

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './_assets/index.css';
+import Home from './features/home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
 );
