5ca0dbdf593c146d166dbb4547d31db9d3a34ad1
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-22 15:50:13 -0300

    create features and inside this file existe more files with on home and product and inside product existe more thinks with one container and pages and actionsm api, reducesm routers

---
+++ src/features/product/containers/ProductList-containers.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function ProductListContainer() {
+  return (
+    <div>
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    </div>
+  );
+}
---
+++ src/features/product/pages/ProductList.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ProductListContainer from '../containers/ProductList-containers';
+
+export default class ProductList extends Component() {
+  render() {
+    return (
+      <div>
+        <ProductListContainer />
+      </div>
+    );
+  }
+}
