fbe5e2c206d37449077e0363e3173237c2323162
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-23 09:00:44 -0300

    rename shopping cart container

---
+++ src/features/product/containers/ShoppingCard-container.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+export default class shoppingCartComponents extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <ul>
+            <ol>Home</ol>
+            <ol>Shopping Cart</ol>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
