4341c6510da4265eb985fbb2c7d742eb43621130
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-23 08:57:27 -0300

    add routers

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './_assets/index.css';
 import Home from './features/home/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Page404 from './features/product/pages/Page404/NotFound';
+import ShoppingCard from './features/product/pages/ShoppingCard';
+
+const routers = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '*',
+    element: <Page404 />,
+  },
+  {
+    path: '/shoppingCart',
+    element: <ShoppingCard />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={routers} />
   </React.StrictMode>,
 );
