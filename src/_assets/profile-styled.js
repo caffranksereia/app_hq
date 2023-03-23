7de74b0996afd2aacfc1fdf72cdd465e5a41421a
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-23 10:06:49 -0300

    modified background yellow for red

---
+++ src/_assets/profile-styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Div = styled.div`
+width: 100%;
+height: 500px:
+background: red;
+`;
+
+export const DivImage = styled.div`
+width: 250px:
+height: 150px;
+background: red;
+`;
