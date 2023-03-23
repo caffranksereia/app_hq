c45669f51ba1bc122ba439944a272455a230025f
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-23 09:54:33 -0300

    add div styled

--- src/features/product/containers/ProductList-containers.jsx
+++ src/features/product/containers/ProductList-containers.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Div } from '../../../_assets/productList-styled';
 export default function ProductListContainer() {
   return (
     <div>
-      <div>
+      <Div>
         <h1>Hello world!</h1>
-      </div>
+      </Div>
     </div>
   );
 }
